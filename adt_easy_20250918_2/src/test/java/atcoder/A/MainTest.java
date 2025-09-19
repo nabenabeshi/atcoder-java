d0db4d19a38d05e1ffd3550878da7d0a1302e86e
Yu Watanabe <nabenabeshi@gmail.com>
2025-09-19 19:42:25 +0900

    AtCoder Daily Training EASY 2025/09/18 18:30start https://atcoder.jp/contests/adt_easy_20250918_2

--- adt_easy_20250918_2/src/test/java/atcoder/A/MainTest.java
+++ adt_easy_20250918_2/src/test/java/atcoder/A/MainTest.java
@@ -9,41 +9,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MainTest {
   @Test
   void test01() {
-    assertEquals("Yes", Main.oneoneslatwotwostring(5, "11/22"));
+    int inputIntArr[][] = {
+      {3,15},
+      {1,15},
+      {3,8},
+    };
+    String expectedStr;
+    expectedStr  = "1 15\r\n";
+    expectedStr += "3 8\r\n";
+    expectedStr += "3 15\r\n";
+    assertEquals(
+      expectedStr,
+      Main.sortDate(inputIntArr));
   }
 
   @Test
   void test02() {
-    assertEquals("Yes", Main.oneoneslatwotwostring(1, "/"));
+    int inputIntArr[][] = {
+        { 1, 1 },
+        { 1, 2 },
+        { 1, 3 },
+        { 1, 4 },
+        { 1, 5 },
+    };
+    String expectedStr = "";
+    expectedStr += "1 1\r\n";
+    expectedStr += "1 2\r\n";
+    expectedStr += "1 3\r\n";
+    expectedStr += "1 4\r\n";
+    expectedStr += "1 5\r\n";
+    assertEquals(
+        expectedStr,
+        Main.sortDate(inputIntArr));
   }
 
   @Test
   void test03() {
-    assertEquals("No", Main.oneoneslatwotwostring(4, "1/22"));
-  }
-
-  @Test
-  void test04() {
-    assertEquals("No", Main.oneoneslatwotwostring(5, "22/11"));
-  }
-
-  @Test
-  void test05() {
-    assertEquals("No", Main.oneoneslatwotwostring(5, "21/12"));
-  }
-
-  @Test
-  void test06() {
-    assertEquals("No", Main.oneoneslatwotwostring(5, "12/21"));
-  }
-
-  @Test
-  void test07() {
-    assertEquals("No", Main.oneoneslatwotwostring(5, "11/21"));
-  }
-
-  @Test
-  void test08() {
-    assertEquals("No", Main.oneoneslatwotwostring(5, "12/22"));
+    int inputIntArr[][] = {
+        { 9, 21},
+        { 8, 22},
+        { 10, 28},
+        { 6, 22},
+        { 6, 26},
+        { 7, 10},
+        { 5, 3 },
+        { 8, 31},
+        { 9, 4 },
+        { 11, 21},
+    };
+    String expectedStr = "";
+    expectedStr += "5 3\r\n";
+    expectedStr += "6 22\r\n";
+    expectedStr += "6 26\r\n";
+    expectedStr += "7 10\r\n";
+    expectedStr += "8 22\r\n";
+    expectedStr += "8 31\r\n";
+    expectedStr += "9 4\r\n";
+    expectedStr += "9 21\r\n";
+    expectedStr += "10 28\r\n";
+    expectedStr += "11 21\r\n";
+    
+    assertEquals(
+        expectedStr,
+        Main.sortDate(inputIntArr));
   }
 }
